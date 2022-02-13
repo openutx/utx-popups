b8b22201b35ffeff46cad675b023bcfb7b0497fb
Lijiawei <1456470136@qq.com>
2022-02-13 15:48:21 +0800

    feat: add default accept buttons text

--- popups/settings.py
+++ popups/settings.py
@@ -15,3 +15,7 @@ class Settings(object):
     SYS = False
     LOOP = 1
     iOS = False
+    DEFAULT_ACCEPT_BUTTONS = [
+        "使用App时允许", "无线局域网与蜂窝网络", "好", "稍后", "稍后提醒", "确定", "允许访问所有照片",
+        "允许", "以后", "打开", "录屏", "Allow", "OK", "YES", "Yes", "Later", "Close"
+    ]
